d34871f1e12bc39f8303bdc302105bd9dcd284ff
Chris Soncrant <christopher.soncrant@gmail.com>
2021-07-23 12:32:01 -0700

    successful programming of the game function

--- script.js
+++ script.js
@@ -23,45 +23,25 @@ function getWinningScore() {
     return parseInt(winningScore);
 }
 
-function test() {
-    nothing = 5;
-}
-
-
 function getUserChoice() {
     let choice = prompt("rock, paper, or scissors?");
+    choice = choice.toLowerCase();
     if (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
-        console.log("Please choose either rock, paper or scissors!");
-        //return getUserChoice();
+        alert("Please choose either rock, paper or scissors!");
+        return getUserChoice();
     }
     return choice;
 }
 
-//Variable Declaration:
-let userScore = 0;
-
-let cpuScore = 0;
-
-let winningScore = 3; //getWinningScore();
-
-let userChoice = "rock" //getUserChoice();
-
-let cpuChoice = "rock"; //getCpuChoice();
-
-let keepGoing = true;
-
-
-
+/*
 function compareChoices() { 
-    
     if (userChoice === "rock") {
         if (cpuChoice === "scissors") {
             userScore++;
-            console.log(userScore);
             if (userScore === winningScore) {
-                console.log(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`); 
+                console.log(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
             } 
-            return `You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`;
+            console.log(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
         }
         if (cpuChoice === "paper") {
             cpuScore++;
@@ -75,8 +55,152 @@ function compareChoices() {
         }
     };
 }
+*/
 
-compareChoices();
+//Variable Declaration:
+/*
+let userScore = 0;
+
+let cpuScore = 0;
+
+let winningScore = 1; //getWinningScore();
+
+let userChoice = "rock" //getUserChoice();
+
+let cpuChoice = "scissors"; //getCpuChoice();
+
+let keepGoing = true;
+*/
+
+
+function game() {
+    let userScore = 0;
+
+    let cpuScore = 0;
+
+    let winningScore = getWinningScore();
+
+    let userChoice;
+
+    let cpuChoice;
+
+    let keepGoing = true;
+
+    function compareChoices() { 
+        userChoice = getUserChoice();
+        cpuChoice = getCpuChoice();
+        alert(`Computer chose ${cpuChoice}`);
+        if (userChoice === "rock") {
+            //User Wins
+            if (cpuChoice === "scissors") {
+                userScore++;
+                if (userScore === winningScore) {
+                    alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //CPU Wins
+            if (cpuChoice === "paper") {
+                cpuScore++;
+                if (cpuScore === winningScore) {
+                    alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //Tie
+            if (cpuChoice === "rock") {
+                alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+        };
+        if (userChoice === "paper") {
+            //User Wins
+            if (cpuChoice === "rock") {
+                userScore++;
+                if (userScore === winningScore) {
+                    alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //CPU Wins
+            if (cpuChoice === "scissors") {
+                cpuScore++;
+                if (cpuScore === winningScore) {
+                    alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //Tie
+            if (cpuChoice === "paper") {
+                alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+        };
+        if (userChoice === "scissors") {
+            //User Wins
+            if (cpuChoice === "paper") {
+                userScore++;
+                if (userScore === winningScore) {
+                    alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //CPU Wins
+            if (cpuChoice === "rock") {
+                cpuScore++;
+                if (cpuScore === winningScore) {
+                    alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                    keepGoing = false;
+                    return;
+                } 
+                alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+            //Tie
+            if (cpuChoice === "scissors") {
+                alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                return;
+            }
+        };
+
+    }
+
+    while(keepGoing) {
+        compareChoices();
+    }
+}
+
+//game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
