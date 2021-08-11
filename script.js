8ccb2d1ba7a0a5e1f2dce52e38af17f2a7cd4dac
Chris Soncrant <christopher.soncrant@gmail.com>
2021-08-10 18:13:03 -0700

    Figured out way to loop back using buttons for the second round

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let cpuChoice;
 
 let winningScore;
 
-
-
 //Independent Functions
 function getCpuChoice() {
     let a = Math.floor(Math.random() * 3);
@@ -28,155 +26,65 @@ function getCpuChoice() {
     };
 }
 
+// function compareChoices() { 
+//     let alert = document.createElement("div");
+//     let textColor;
+//     let nextBtn = document.createElement("button");
+//     nextBtn.classList.add("btn-div");
+//     nextBtn.textContent = "Next round";
+//     nextBtn.addEventListener('click', () => {
+//         removeChoiceBtns.forEach(a => {
+//             a.style.display = "inline";
+//         })
+//     })
 
+//     console.log(winningScore);
 
-//ORIGINAL COMPARECHOICES FUNCTION WITHOUT UI:
-function compareChoices() { 
-    userChoice = getUserChoice();
-    cpuChoice = getCpuChoice();
-    alert(`Computer chose ${cpuChoice}`);
-    if (userChoice === "rock") {
-        //User Wins
-        if (cpuChoice === "Scissors") {
-            userScore++;
-            if (userScore === winningScore) {
-                alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //CPU Wins
-        if (cpuChoice === "Paper") {
-            cpuScore++;
-            if (cpuScore === winningScore) {
-                alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //Tie
-        if (cpuChoice === "Rock") {
-            alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-    };
-    if (userChoice === "paper") {
-        //User Wins
-        if (cpuChoice === "Rock") {
-            userScore++;
-            if (userScore === winningScore) {
-                alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //CPU Wins
-        if (cpuChoice === "Scissors") {
-            cpuScore++;
-            if (cpuScore === winningScore) {
-                alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //Tie
-        if (cpuChoice === "Paper") {
-            alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-    };
-    if (userChoice === "scissors") {
-        //User Wins
-        if (cpuChoice === "Paper") {
-            userScore++;
-            if (userScore === winningScore) {
-                alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //CPU Wins
-        if (cpuChoice === "Rock") {
-            cpuScore++;
-            if (cpuScore === winningScore) {
-                alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //Tie
-        if (cpuChoice === "Scissors") {
-            alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-    };
-}
+//     function appendAlert(message) {
+//         alert.textContent = message;
+//         document.querySelector("#choice-display").style.display = "none";
+//         alert.style.color = textColor;
+//         document.querySelector(".choices-btn-div").appendChild(alert);
+//         setTimeout(() => {
+//             document.querySelector(".choices-btn-div").appendChild(nextBtn);
+//         }, 1200)
+//     }
 
-//NEW COMPARECHOICES WITH UI FUNCTIONALITY:
-function compareChoices() { 
-    let alert = document.createElement("div");
-    let textColor;
-    let nextBtn = document.createElement("button");
-    nextBtn.classList.add("btn-div");
-    nextBtn.textContent = "Next round";
-    console.log(winningScore);
+//     if (userChoice === "Rock") {
+//         //User Wins
+//         if (cpuChoice === "Scissors") {
+//             userScore++;
+//             document.getElementById("user-score").textContent = userScore;
+//             if (userScore === winningScore) {
+//                 /* alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
+//                 keepGoing = false;
+//                 return; */
+//             } 
+//             let message = "You won this round!";
+//             textColor = "green";
+//             appendAlert(message);
+//             /* alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+//             return; */
+//         }
+//         //CPU Wins
+//         if (cpuChoice === "Paper") {
+//             cpuScore++;
+//             if (cpuScore === winningScore) {
+//                 alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
+//                 keepGoing = false;
+//                 return;
+//             } 
+//             alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+//             return;
+//         }
+//         //Tie
+//         if (cpuChoice === "Rock") {
+//             alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+//             return;
+//         }
+//     };
+// }
 
-    function appendAlert(message) {
-        alert.textContent = message;
-        document.querySelector("#choice-display").style.display = "none";
-        alert.style.color = textColor;
-        document.querySelector(".choices-btn-div").appendChild(alert);
-        setTimeout(() => {
-            document.querySelector(".choices-btn-div").appendChild(nextBtn);
-        }, 1200)
-    }
-
-    if (userChoice === "Rock") {
-        //User Wins
-        if (cpuChoice === "Scissors") {
-            userScore++;
-            document.getElementById("user-score").textContent = userScore;
-            if (userScore === winningScore) {
-                /* alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return; */
-            } 
-            let message = "You won this round!";
-            textColor = "green";
-            appendAlert(message);
-            /* alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return; */
-        }
-        //CPU Wins
-        if (cpuChoice === "Paper") {
-            cpuScore++;
-            if (cpuScore === winningScore) {
-                alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
-                keepGoing = false;
-                return;
-            } 
-            alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-        //Tie
-        if (cpuChoice === "Rock") {
-            alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
-            return;
-        }
-    };
-}
 
 //Start Button:
 let startButton = document.querySelector("#start-btn");
@@ -215,8 +123,8 @@ function moveToUserChoice() {
 
         let removeChoiceBtns = document.querySelectorAll(".choice-btn");
         
-        removeChoiceBtns.forEach(training => {
-            training.remove();
+        removeChoiceBtns.forEach(a => {
+            a.style.display = "none";
         })
 
         document.getElementById("choice-title").textContent = "";
@@ -224,18 +132,74 @@ function moveToUserChoice() {
         document.querySelector("#cpu-choice").textContent = `The computer chose: ${cpuChoice}`;
         document.querySelector("#choice-display").style.display = "block";
 
+        function compareChoices() { 
+            let alert = document.createElement("div");
+            let textColor;
+            let nextBtn = document.createElement("button");
+            nextBtn.classList.add("btn-div");
+            nextBtn.textContent = "Next round";
+            nextBtn.addEventListener('click', () => {
+                removeChoiceBtns.forEach(a => {
+                    document.getElementById("choice-title").textContent = "Rock, Paper, or Scissors?";
+                    a.style.display = "inline";
+                    alert.remove();
+                    nextBtn.remove();
+                })
+            })
+
+            console.log(winningScore);
+
+            function appendAlert(message) {
+                alert.textContent = message;
+                document.querySelector("#choice-display").style.display = "none";
+                alert.style.color = textColor;
+                document.querySelector(".choices-btn-div").appendChild(alert);
+                setTimeout(() => {
+                    document.querySelector(".choices-btn-div").appendChild(nextBtn);
+                }, 1200)
+            }
+
+            if (userChoice === "Rock") {
+                //User Wins
+                if (cpuChoice === "Scissors") {
+                    userScore++;
+                    document.getElementById("user-score").textContent = userScore;
+                    if (userScore === winningScore) {
+                        /* alert(`You win! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                        keepGoing = false;
+                        return; */
+                    } 
+                    let message = "You won this round!";
+                    textColor = "green";
+                    appendAlert(message);
+                    /* alert(`You won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                    return; */
+                }
+                //CPU Wins
+                if (cpuChoice === "Paper") {
+                    cpuScore++;
+                    if (cpuScore === winningScore) {
+                        alert(`The CPU wins! Final score: User: ${userScore} CPU: ${cpuScore}`);
+                        keepGoing = false;
+                        return;
+                    } 
+                    alert(`The CPU won this round! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                    return;
+                }
+                //Tie
+                if (cpuChoice === "Rock") {
+                    alert(`Tie! Your score: ${userScore}. CPU score: ${cpuScore}.`);
+                    return;
+                }
+            };
+         }
+
         setTimeout(() => {
             compareChoices();
         }, 1200);
     }
 
 }
-
-
-document.querySelector("button").addEventListener('click', game);
-
-
-
 
 
 
